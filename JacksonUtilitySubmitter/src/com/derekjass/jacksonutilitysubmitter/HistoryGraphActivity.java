c6dfa94e4a887f215eaee13435e7b07be532e914
Derek Jass <jazzer36@gmail.com>
2013-12-05 22:21:31 -0600

    BarView made.

--- JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/HistoryGraphActivity.java
+++ JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/HistoryGraphActivity.java
@@ -1,34 +1,36 @@
 package com.derekjass.jacksonutilitysubmitter;
 
-import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.graphics.LightingColorFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+
+import com.derekjass.jacksonutilitysubmitter.views.BarView;
 
 public class HistoryGraphActivity extends ActionBarActivity {
 
-	private View mBar;
+	private BarView mBar;
+	private BarView mBar2;
 
-	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_history_graph);
 
-		mBar = findViewById(R.id.bar);
-		mBar.getBackground().setColorFilter(
+		mBar = (BarView) findViewById(R.id.bar);
+		mBar.getBackground().mutate().setColorFilter(
 				new LightingColorFilter(Color.GREEN, 0));
+
+		mBar2 = (BarView) findViewById(R.id.bar2);
+		mBar2.getBackground().mutate().setColorFilter(
+				new LightingColorFilter(Color.BLUE, 0));
 	}
 
-	@SuppressLint("NewApi")
 	public void animateBar(View v) {
-		mBar.setPivotY(mBar.getHeight());
-		mBar.animate()
-		.setInterpolator(new AccelerateDecelerateInterpolator())
-		.scaleY((float) (Math.random()));
+		mBar.setScale((float) Math.random());
+
+		mBar2.setScale((float) Math.random());
 	}
 }
--- JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/views/BarView.java
+++ JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/views/BarView.java
@@ -3,12 +3,45 @@ package com.derekjass.jacksonutilitysubmitter.views;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
 
 public class BarView extends View {
 
-	public BarView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		// TODO Auto-generated constructor stub
+	private float mScale;
+
+	public BarView(Context context) {
+		this(context, null);
 	}
 
+	public BarView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		Animation anim = new ScaleAnimation(
+				1f, 1f,
+				0f, 0f,
+				Animation.RELATIVE_TO_SELF, 0.5f,
+				Animation.RELATIVE_TO_SELF, 1f);
+		anim.setFillAfter(true);
+		anim.setDuration(0);
+		startAnimation(anim);
+		mScale = 0f;
+	}
+
+	public void setScale(float scale) {
+		Animation anim = new ScaleAnimation(
+				1f, 1f,
+				mScale, scale,
+				Animation.RELATIVE_TO_SELF, 0.5f,
+				Animation.RELATIVE_TO_SELF, 1f);
+		anim.setFillAfter(true);
+		anim.setDuration(400);
+		anim.setInterpolator(getContext(),
+				android.R.anim.accelerate_decelerate_interpolator);
+		startAnimation(anim);
+		mScale = scale;
+	}
+
+	public float getScale() {
+		return mScale;
+	}
 }
