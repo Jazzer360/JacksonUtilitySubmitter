5256b09cd666726f86bbb7b1fc9dd71702276f17
Derek Jass <jazzer36@gmail.com>
2013-12-08 11:28:32 -0600

    Added empty BarGraph view

---
+++ JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/views/BarGraph.java
@@ -0,0 +1,24 @@
+package com.derekjass.jacksonutilitysubmitter.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class BarGraph extends ViewGroup {
+
+	public BarGraph(Context context) {
+		this(context, null);
+	}
+	
+	public BarGraph(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
