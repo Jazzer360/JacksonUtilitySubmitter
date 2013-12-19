7b249da7318547ac7f4a3b1c8dc1a4f3f28b308a
Derek Jass <jazzer36@gmail.com>
2013-12-19 14:25:58 -0600

    UsageStatistics class pretty much done

--- JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/util/UsageStatistics.java
+++ JacksonUtilitySubmitter/src/com/derekjass/jacksonutilitysubmitter/util/UsageStatistics.java
@@ -1,58 +1,62 @@
 package com.derekjass.jacksonutilitysubmitter.util;
 
-import java.util.TreeSet;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 import android.database.Cursor;
 
 public class UsageStatistics {
 
-	private static class Entry implements Comparable<Entry> {
+	private NavigableMap<Long, Integer> mReadings;
 
-		private long mTime;
-		private int mReading;
-
-		private Entry(long time, int reading) {
-			mTime = time;
-			mReading = reading;
-		}
-
-		@Override
-		public int compareTo(Entry another) {
-			if (mTime == another.mTime) return 0;
-			return (mTime < another.mTime) ? -1 : 1;
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + mReading;
-			result = prime * result + (int) (mTime ^ (mTime >>> 32));
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) return true;
-			if (obj == null) return false;
-			if (!(obj instanceof Entry)) return false;
-			Entry other = (Entry) obj;
-			if (mReading != other.mReading) return false;
-			if (mTime != other.mTime) return false;
-			return true;
-		}
-	}
-
-	private TreeSet<Entry> mReadings;
-
-	public UsageStatistics(Cursor data, int dateCol, int readingCol) {
-		mReadings = new TreeSet<Entry>();
+	public UsageStatistics(Cursor data, int timeColIndex, int valueColIndex) {
+		mReadings = new TreeMap<Long, Integer>();
 		if (!data.moveToFirst()) return;
 		do {
-			long time = data.getLong(dateCol);
-			int reading = data.getInt(readingCol);
-			mReadings.add(new Entry(time, reading));
+			long time = data.getLong(timeColIndex);
+			int reading = data.getInt(valueColIndex);
+			mReadings.put(time, reading);
 		} while (data.moveToNext());
 	}
 
+	public int getUsage(long from, long to) {
+		if (mReadings.isEmpty() ||
+				from < mReadings.firstKey() ||
+				to > mReadings.lastKey()) return 0;
+
+		float totalUsage = 0f;
+
+		Entry<Long, Integer> fromEntry = mReadings.floorEntry(from);
+		Entry<Long, Integer> toEntry = mReadings.higherEntry(from);
+
+		do {
+			if (fromEntry.getKey() < from || toEntry.getKey() > to) {
+				long span = toEntry.getKey() - fromEntry.getKey();
+				int usage = getUnitsUsed(
+						fromEntry.getValue(), toEntry.getValue());
+				float rate = (float) usage / span;
+
+				totalUsage += rate * (Math.min(toEntry.getKey(), to) -
+						Math.max(fromEntry.getKey(), from));
+			} else {
+				totalUsage += getUnitsUsed(
+						fromEntry.getValue(), toEntry.getValue());
+			}
+
+			fromEntry = toEntry;
+			toEntry = mReadings.higherEntry(fromEntry.getKey());
+		} while (fromEntry.getKey() < to);
+
+		return (int) totalUsage;
+	}
+
+	private int getUnitsUsed(int start, int end) {
+		if (end > start) return end - start;
+		int meterDigits = 1;
+		int lastReading = start;
+		while ((lastReading /= 10) > 0) meterDigits++;
+		int meterCapacity = (int) Math.pow(10, meterDigits);
+		return meterCapacity - start + end;
+	}
 }
